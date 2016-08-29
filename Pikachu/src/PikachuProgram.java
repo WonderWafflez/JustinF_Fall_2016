da392b55f97ee70c9bbcbdad74b5b7ce49446a33
WonderWafflez <justinaptx4869@gmail.com>
2016-08-29 08:41:59 -0700

    Pikachu

---
+++ Pikachu/src/PikachuProgram.java
@@ -0,0 +1,9 @@
+
+public class PikachuProgram {
+	public static void main(String[] args){
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
+	}
+
+}
